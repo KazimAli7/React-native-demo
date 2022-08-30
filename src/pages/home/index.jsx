f961c52c29076afafe695deeca1baa8bfec2f413
Kazim Agha <kazim.agha@invozone.com>
2022-08-30 12:35:45 +0500

    Pushing changes to master

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const HomePage = () => {
+    return(
+        <View>
+            <Text>
+                Hello world!
+            </Text>
+        </View>
+    )
+}
+
+export default HomePage;
